379aa612f9c8397a9258b4848b6cf1d261b7062c
phanuwit.h <phanuwit.h@gmail.com>
2024-03-05 16:51:52 +0700

    serach & select only match

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,8 @@
-import {createApp } from 'vue';
+import { createApp } from 'vue'
 
-import VueTreeSelect from '../dist/vue3-treeselect.common';
-import '../dist/vue3-treeselect.css';
+import VueTreeSelect from '../dist/vue3-treeselect.common'
+import '../dist/vue3-treeselect.css'
 
+import App from './demo-app.vue'
 
-import App from './demo-app.vue';
-
-createApp(App)
-    .component('vue-treeselect',VueTreeSelect)
-    .mount('#app');
+createApp(App).component('vue-treeselect', VueTreeSelect).mount('#app')
